0d404b2af0947a6bcc883d9e75168da80ba4c855
yuroweb55 <168056659+yuroweb55@users.noreply.github.com>
2024-10-21 21:35:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const request = require('request');
+const cors = require('cors');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.get('/proxy', (req, res) => {
   const url = req.query.url;
   if (!url) {
