01521fff650121ce4c279bf01cd8a6aead332019
yuroweb55 <168056659+yuroweb55@users.noreply.github.com>
2024-11-11 12:04:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get('/vimg', async (req, res,next) => {
                     
                     // ใช้ sharp เพื่อบีบอัดภาพใน memory และแปลงเป็น WebP
                     const webpBuffer = await sharp(imageBuffer)
-                        .webp({ quality: 40 })
+                        .webp({ quality: 60 })
                         .toBuffer();
                     
                     res.set('Content-Type', 'image/webp');
