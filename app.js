13e13199b08f161ee03b78a707a3a47268722edb
yuroweb55 <168056659+yuroweb55@users.noreply.github.com>
2025-03-07 06:30:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const compression = require('compression');
 const cors = require('cors');
-const https = require('https');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
@@ -21,54 +20,6 @@ app.use(
       },
     })
   );
-
-app.get('/httpsvideo', (req, res) => {
-    const videoUrl = req.query.url;
-
-    if (!videoUrl) {
-        return res.status(400).json({ error: 'Video URL is required' });
-    }
-
-    try {
-        const decodedUrl = decodeURIComponent(videoUrl);
-        const range = req.headers.range || 'bytes=0-';
-
-        const headers = {
-            'accept': '*/*',
-            'accept-language': 'th',
-            'range': range,
-            'referer': 'https://www.youtube.com/',
-            'referrer-policy': 'strict-origin-when-cross-origin',
-        };
-
-        https.get(decodedUrl, { headers }, (videoRes) => {
-            const { statusCode, headers: videoHeaders } = videoRes;
-
-            if (statusCode >= 200 && statusCode < 300) {
-                res.writeHead(statusCode, {
-                    'Content-Range': videoHeaders['content-range'] || '',
-                    'Accept-Ranges': 'bytes',
-                    'Content-Type': videoHeaders['content-type'] || 'application/octet-stream',
-                    'Cache-Control': 'public, max-age=31536000',
-                    'Connection': 'keep-alive',
-                });
-
-                // ส่งข้อมูลไปยัง client
-                videoRes.pipe(res, { end: true });
-            } else {
-                res.status(statusCode).send('Failed to fetch video');
-            }
-        }).on('error', (err) => {
-            console.error('Error fetching video:', err.message);
-            res.status(502).json({ error: 'Failed to fetch video' });
-        });
-    } catch (error) {
-        console.error('Unexpected error:', error.message);
-        res.status(500).json({ error: 'Unexpected server error' });
-    }
-});
-
-
 
 
 const PORT = process.env.PORT || 3500;
